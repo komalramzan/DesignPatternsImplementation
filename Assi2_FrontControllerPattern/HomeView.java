4d2f898c81008c56de4330da0d66d439970947b8
komalramzan <73910566+komalramzan@users.noreply.github.com>
2021-12-24 02:52:33 +0500

    Add files via upload

---
+++ Assi2_FrontControllerPattern/HomeView.java
@@ -0,0 +1,7 @@
+package Assi2_FrontControllerPattern;
+
+public class HomeView {
+	   public void show(){
+	      System.out.println("Displaying Home Page");
+	   }
+	}
---
+++ Assi2_FrontControllerPattern/StudentView.java
@@ -0,0 +1,7 @@
+package Assi2_FrontControllerPattern;
+
+public class StudentView {
+	   public void show(){
+	      System.out.println("Displaying Student Page");
+	   }
+	}
